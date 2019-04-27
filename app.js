f2d3e6db3a9f068f694aa1c8da86a34ac7d1cdfb
Akinari Tsugo <garafo@gmail.com>
2019-04-27 11:58:40 +0900

    First commit.

---
+++ app.js
@@ -0,0 +1,10 @@
+var path = require("path");
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+app.use("/public", express.static(path.join(__dirname, "/public")));
+app.use(require("./routes/index.js"));
+
+app.listen(3000);
